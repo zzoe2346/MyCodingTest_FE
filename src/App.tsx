abbb69c9f1b496a3abf5883a13c4ed553cbc9276
Seonghun Jeong <tjdgns5506@gmail.com>
2025-01-09 16:50:57 +0900

    feat: App.tsx 에 테마 추가, 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,79 @@ import Review from './pages/Review'
 import AuthPage from './pages/AuthPage'
 import {AuthProvider} from "./context/AuthContext.tsx";
 import PrivateRoute from "./components/PrivateRoute.tsx";
-import SolveProblemListPage from "./pages/SolvedProblemListPage.tsx";
+import SolveProblemListPage from "./pages/AllSolvedProblemPage.tsx";
+import {createTheme, ThemeProvider} from "@mui/material";
+import FavoirteSolvedProblem from "./pages/FavoriteSolvedProblemPage.tsx";
+import ReviewClearSolvedProblemPage from "./pages/ReviewClearSolvedProblemPage.tsx";
+import ReviewUnClearSolvedProblemPage from "./pages/ReviewUnClearSolvedProblemPage.tsx";
+
+const theme = createTheme({
+    components: {
+        MuiTableCell: {
+            styleOverrides: {
+                root: {
+                    textAlign: 'center',
+                },
+            },
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    padding: '4px 8px', // 기본 패딩 조정
+                    minWidth: 'auto', // 기본 최소 너비 제거
+                    fontSize: '1.0rem', // 텍스트 크기
+                },
+            },
+        },
+    },
+
+
+});
+
 
 function App() {
     return (
-        <AuthProvider>
-            <MyAppBar/>
-            <Routes>
-                <Route path="/login" element={<AuthPage/>}/>
-                <Route path="/solvedProblems" element={<SolveProblemListPage/>}/>
-                <Route
-                    path="/review"
-                    element={
-                        <PrivateRoute>
-                            <Review/>
-                        </PrivateRoute>
-                    }
-                />
-            </Routes>
-        </AuthProvider>
+        <ThemeProvider theme={theme}>
+            <AuthProvider>
+                <MyAppBar/>
+                <Routes>
+                    <Route path="/login" element={<AuthPage/>}/>
+                    <Route path="/solvedProblems" element={<SolveProblemListPage/>}/>
+                    <Route
+                        path="/review"
+                        element={
+                            <PrivateRoute>
+                                <Review/>
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/favorite"
+                        element={
+                            <PrivateRoute>
+                                <FavoirteSolvedProblem/>
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/review-clear"
+                        element={
+                            <PrivateRoute>
+                                <ReviewClearSolvedProblemPage/>
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/review-unclear"
+                        element={
+                            <PrivateRoute>
+                                <ReviewUnClearSolvedProblemPage/>
+                            </PrivateRoute>
+                        }
+                    />
+                </Routes>
+            </AuthProvider>
+        </ThemeProvider>
     )
 }
 
