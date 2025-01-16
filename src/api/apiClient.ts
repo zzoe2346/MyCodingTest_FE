6c0d48d85274586b4d048d217b9a670a218fa96d
Seonghun Jeong <tjdgns5506@gmail.com>
2025-01-16 16:51:05 +0900

    chore: 운영용 URL 연결

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -3,8 +3,13 @@ import axios from 'axios';
 axios.defaults.withCredentials = true
 
 const apiClient = axios.create({
+    // for DEV
     // baseURL: 'https://localhost:8443',
-    baseURL: 'http://localhost:8080',
+    // baseURL: 'http://localhost:8080',
+
+    // for OPERATION
+    baseURL: 'https://api.mycodingtest.com',
+
     withCredentials: true
 });
 
