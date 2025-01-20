f47aa528d7713f7eee349c5be7f8a853be49ae6d
Seonghun Jeong <tjdgns5506@gmail.com>
2025-01-20 19:47:14 +0900

    feat: 백준 코드 언어 파싱 함수 추가

--- src/components/CodeArea.tsx
+++ src/components/CodeArea.tsx
@@ -1,8 +1,91 @@
 import {CircularProgress, Paper, Typography} from '@mui/material';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
-import {materialLight} from 'react-syntax-highlighter/dist/esm/styles/prism';
 import apiClient from "../api/apiClient.ts";
 import {useEffect, useState} from "react";
+import {oneLight} from "react-syntax-highlighter/dist/cjs/styles/prism";
+
+function mapLanguageToPrism(language: string): string {
+    const languageMap: { [key: string]: string } = {
+        "C++17": "cpp",
+        "Java 8": "java",
+        "Python 3": "python",
+        "C11": "c",
+        "PyPy3": "python",
+        "C99": "c",
+        "C++98": "cpp",
+        "C++11": "cpp",
+        "C++14": "cpp",
+        "Java 8 (OpenJDK)": "java",
+        "Java 11": "java",
+        "C++20": "cpp",
+        "C++23": "cpp",
+        "C++26": "cpp",
+        "Ruby": "ruby",
+        "Kotlin (JVM)": "kotlin",
+        "Swift": "swift",
+        "C#": "csharp",
+        "node.js": "javascript",
+        "Go": "go",
+        "Go (gccgo)": "go",
+        "Java 15": "java",
+        "D": "d",
+        "D (LDC)": "d",
+        "PHP": "php",
+        "Rust 2015": "rust",
+        "Rust 2018": "rust",
+        "Rust 2021": "rust",
+        "Pascal": "pascal",
+        "Lua": "lua",
+        "Perl": "perl",
+        "F#": "fsharp",
+        "Visual Basic": "vbnet",
+        "Objective-C": "objectivec",
+        "Objective-C++": "objectivec",
+        "C99 (Clang)": "c",
+        "C++98 (Clang)": "cpp",
+        "C++11 (Clang)": "cpp",
+        "C++14 (Clang)": "cpp",
+        "C11 (Clang)": "c",
+        "C++17 (Clang)": "cpp",
+        "C++20 (Clang)": "cpp",
+        "Golfscript": "golfscript",
+        "C90": "c",
+        "C2x": "c",
+        "C90 (Clang)": "c",
+        "C2x (Clang)": "c",
+        "TypeScript": "typescript",
+        "Assembly (32bit)": "asm32",
+        "Assembly (64bit)": "asm64",
+        "Bash": "bash",
+        "Fortran": "fortran",
+        "Scheme": "scheme",
+        "Ada": "ada",
+        "awk": "awk",
+        "OCaml": "ocaml",
+        "Brainf**k": "brainfuck",
+        "Whitespace": "whitespace",
+        "Tcl": "tcl",
+        "Rhino": "javascript",
+        "Pike": "pike",
+        "sed": "sed",
+        "INTERCAL": "intercal",
+        "bc": "bc",
+        "Algol 68": "algol68",
+        "Befunge": "befunge",
+        "FreeBASIC": "basic",
+        "Haxe": "haxe",
+        "LOLCODE": "lolcode",
+        "아희": "ahk",
+        "SystemVerilog": "systemverilog",
+    };
+
+    if (languageMap[language]) {
+        return languageMap[language];
+    } else {
+        console.warn(`Language not found in map: ${language}`);
+        return "";
+    }
+}
 
 interface CodeAreaProps {
     judgmentResultId: number;
@@ -48,7 +131,8 @@ function CodeArea({judgmentResultId, language}: CodeAreaProps) {
             ) : error ? (
                 <Typography color="error">{error}</Typography>
             ) : (
-                <SyntaxHighlighter language={language} style={materialLight}>
+                <SyntaxHighlighter language={mapLanguageToPrism(language)} style={oneLight} showLineNumbers
+                                   lineNumberStyle={{minWidth: '25px', padding: '0 2', textAlign: 'right'}}>
                     {code}
                 </SyntaxHighlighter>
             )}
