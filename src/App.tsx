ae3ddb944bd84154afd6c6c30d473f0cdb155b7d
Seonghun Jeong <tjdgns5506@gmail.com>
2025-01-26 21:31:50 +0900

    fix: 긴급 원복

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,8 @@ import TagPage from "./pages/TagPage.tsx";
 import {SnackbarProvider} from "notistack";
 import HomePage from "./pages/HomePage.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
-import {NavigateSetter} from "./api/apiClient.ts";
+
+// import {NavigateSetter} from "./api/apiClient.ts";
 
 
 function App() {
@@ -63,7 +64,7 @@ function App() {
                 }}
                 >
                     <MyAppBar/>
-                    <NavigateSetter/>
+                    {/*<NavigateSetter/>*/}
                     <Routes>
                         <Route path="/review/:reviewId" element={<ReviewPage/>}/>
                         <Route path="/error" element={<ErrorPage/>}/>
